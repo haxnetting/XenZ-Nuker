c7119d9ae5b2c3f0822a55ef59256e6e5ab86b45
The Netting Guy <87181561+haxnetting@users.noreply.github.com>
2024-12-27 13:32:50 +0100

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,12 @@
+@echo off
+
+echo Installing discord.py...
+pip install discord.py
+
+echo Installing colorama...
+pip install colorama
+
+echo Installing aiohttp...
+pip install aiohttp
+
+echo Dependencies installed successfully!
